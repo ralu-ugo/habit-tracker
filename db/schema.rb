36eb9dcd2c3862d2eff39da02130d852a2fc8099
Sam Hillman <sam.hillman399@gmail.com>
2023-03-06 15:21:41 +0000

    habits

---
+++ db/schema.rb
@@ -0,0 +1,35 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_151615) do
+  create_table "habits", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "favourite"
+    t.integer "user_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "repeat"
+    t.boolean "monday"
+    t.boolean "tuesday"
+    t.boolean "wednesday"
+    t.boolean "thursday"
+    t.boolean "friday"
+    t.boolean "saturday"
+    t.boolean "sunday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_habits_on_user_id"
+  end
+
+  add_foreign_key "habits", "users"
+end
